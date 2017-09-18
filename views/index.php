04f5784e40ae4da7e27a1b97282c463036af42cd
Anton Semin <semin_tosha@bk.ru>
2017-09-18 19:11:46 +0300

    Добавил ссылку на id статьи

--- views/index.php
+++ views/index.php
@@ -5,13 +5,15 @@
         <meta charset="UTF-8">
     </head>
     <body>
-        <h1>News of the world!<h1>
+        <h1>News of the world!</h1>
+
         <?php
            foreach($articles as $article) {
                ?>
-               <p><?=$article?></p>
-               <?php
-           }
+               <h4><a href="article_page.php?id='<?=$article['id']?>'"><?=$article['title']."&nbsp;&nbsp;".$article['time']?></a></h4>
+
+        <?
+             }
         ?>
     </body>
 </html>
